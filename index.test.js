0092996e3cbeb56f19c71ebdeb47b1f45618e844
Lukas <lukasdrinkwater97@gmail.com>
2024-01-31 11:11:45 +0000

    commit before removing from local

--- index.test.js
+++ index.test.js
@@ -1,32 +1,8 @@
-const {
-  capitalise,
-  reverseString,
-  Calc,
-  caesarCipher,
-  analyseArray,
-} = require("./index");
+const {} = require("./index");
 
 describe("my stack", () => {
   it("capitalise works", () => {
     expect(capitalise("test")).toBe("Test");
-  });
-
-  it("reverse string works", () => {
-    expect(reverseString("test")).toBe("tset");
-  });
-
-  it("test calc class", () => {
-    const calc = new Calc();
-
-    expect(calc.add(1, 1)).toBe(2);
-    expect(calc.subtract(2, 1)).toBe(1);
-    expect(calc.divide(10, 5)).toBe(2);
-    expect(calc.multiply(3, 5)).toBe(15);
-  });
-
-  it("test cihper", () => {
-    expect(caesarCipher("test", 0)).toBe("uftu");
-    expect(caesarCipher("test!", 0)).toBe("uftu!");
   });
 
   it("test analyseArray", () => {
@@ -40,25 +16,3 @@ describe("my stack", () => {
     expect(analyseArray([1, 2, 3, 4, 5])).toMatchObject;
   });
 });
-
-// describe("my stack", () => {
-//   let stack;
-
-//   beforeEach(() => {
-//     stack = new Stack();
-//   });
-
-//   it("is created empty", () => {
-//     expect(stack.top).toBe(-1);
-//     expect(stack.items).toEqual({});
-//   });
-
-//   it("can push to the top", () => {
-//     stack.push("testpush");
-
-//     expect(stack.top).toBe(0);
-//     expect(stack.peek).toBe("testpush");
-//   });
-
-//   it.todo("can pop off");
-// });
